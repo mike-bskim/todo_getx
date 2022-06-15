71ce9ed0ca0e9fae06c7afbef848ffb7259dfbc2
mike.bskim <mike.bskim@gmail.com>
2022-06-15 23:52:36 +0200

    05_edit&delete

--- lib/screens/todos_screen.dart
+++ lib/screens/todos_screen.dart
@@ -10,19 +10,23 @@ class TodosScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-            child: Column(
-              children: const [
-                TodoHeader(),
-                CreateTodo(),
-                SizedBox(height: 20.0),
-                SearchAndFilterTodo(),
-                ShowTodos(),
-              ],
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  TodoHeader(),
+                  CreateTodo(),
+                  SizedBox(height: 20.0),
+                  SearchAndFilterTodo(),
+                  ShowTodos(),
+                ],
+              ),
             ),
           ),
         ),
@@ -90,10 +94,7 @@ class _CreateTodoState extends State<CreateTodo> {
 }
 
 class SearchAndFilterTodo extends StatelessWidget {
-  //StatelessWidget
   const SearchAndFilterTodo({Key? key}) : super(key: key);
-
-  // String clickedType = 'all';
 
   // final debounce = Debounce(milliseconds: 1000);
   @override
@@ -130,7 +131,6 @@ class SearchAndFilterTodo extends StatelessWidget {
   }
 
   Widget filterButton(BuildContext context, Filter filter) {
-    //, Filter filter
     return TextButton(
       onPressed: () {
         // 일반적으로는 함수처리해야 함. 직접 접근하지 말것
@@ -155,7 +155,6 @@ class SearchAndFilterTodo extends StatelessWidget {
   }
 
   Color textColor(BuildContext context, Filter filter) {
-    //Filter filter
     final currentFilter = TodosFilter.to.todosFilter;
     return currentFilter.value == filter ? Colors.blue : Colors.grey;
   }
@@ -185,11 +184,14 @@ class ShowTodos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final todos = context.watch<FilteredTodos>().state.filteredTodos;
     final currentTodos = FilteredTodos.to.filteredTodos;
 
     return Obx(() {
       return ListView.separated(
+        // 에러가 발생(Vertical viewport was given unbounded height.)하므로
+        // 1. 아래 2개의 조건을 줘야 함. primary & shrinkWrap. 이건 전체가 바운딩처리됨.
+        // 2. Expanded 조건으로 감싸면 화면전체가 바운딩도지 않고 ShowTodos 만 바운디처리됨
+        //    2번으로 할때, 외부에 SingleChildScrollView 가 있으면 2번은 동작하지 않음.
         primary: false,
         shrinkWrap: true,
         itemCount: currentTodos.length,
@@ -197,29 +199,98 @@ class ShowTodos extends StatelessWidget {
           return const Divider(color: Colors.grey);
         },
         itemBuilder: (BuildContext context, int index) {
-          return TodoItem(todo: currentTodos[index]);
+          return Dismissible(
+              key: ValueKey(currentTodos[index].id),
+              onDismissed: (_) {
+                TodosList.to.deleteTodo(id: currentTodos[index].id);
+              },
+              confirmDismiss: (_) {
+                return Get.defaultDialog(
+                  title: 'Are you sure?',
+                  middleText: 'Do you really want to delete',
+                  actions: [
+                    ElevatedButton(
+                      child: const Text('YES'),
+                      onPressed: () {
+                        return Get.back(result: true);
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('NO'),
+                      onPressed: () {
+                        return Get.back(result: false);
+                      },
+                    ),
+                  ],
+                );
+              },
+              child: TodoItem(todo: currentTodos[index]));
         },
       );
     });
   }
 }
 
-class TodoItem extends StatelessWidget {
+class TodoItem extends StatefulWidget {
   final Todo todo;
 
   const TodoItem({Key? key, required this.todo}) : super(key: key);
 
   @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  final itemFocusNode = FocusNode();
+  final textFieldFocusNode = FocusNode();
+  final textEditingController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-        value: todo.completed,
-        onChanged: (bool? checked) {
-          debugPrint('clicked toggle button~~');
-          TodosList.to.toggleTodo(id: todo.id);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 1),
+      child: Focus(
+        focusNode: itemFocusNode,
+        onFocusChange: (value) {
+          // if value is true, it get the focus
+          if (value) {
+            textEditingController.text = widget.todo.desc;
+            debugPrint('got the focus: "${widget.todo.desc}"');
+          } else {
+            TodosList.to.editTodo(
+              id: widget.todo.id,
+              desc: textEditingController.text,
+            );
+            debugPrint('lost the focus: "${widget.todo.desc}"');
+          }
         },
+        child: ListTile(
+          onTap: () {
+            debugPrint('click for editing~~');
+            setState(() {
+              // 이건 focus 취득 및 타이틀을 텍스트 또는 텍스트필드로 변환하는 기능
+              itemFocusNode.requestFocus();
+              // 아래는 수정시, 텍스트필드의 autofocus 및 키보드 자동로딩용
+              // 없어도 동작에는 문제가 없음, 약간 불편한 화면처리 정도임
+              textFieldFocusNode.requestFocus();
+            });
+          },
+          leading: Checkbox(
+            value: widget.todo.completed,
+            onChanged: (bool? checked) {
+              debugPrint('clicked toggle button~~');
+              TodosList.to.toggleTodo(id: widget.todo.id);
+            },
+          ),
+          title: itemFocusNode.hasFocus
+              ? TextField(
+                  controller: textEditingController,
+                  autofocus: true,
+                  focusNode: textFieldFocusNode,
+                )
+              : Text(widget.todo.desc),
+        ),
       ),
-      title: Text(todo.desc),
     );
   }
 }
